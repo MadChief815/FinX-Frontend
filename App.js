40510831568858d09c4d0ed7d1e7509b059c84e4
Deshan <deshananuruddha815@gmail.com>
2026-09-22 13:33:19 +0530

    Navigation: File names updated

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import RootNavigator from './navigation/RootNavigator';
+import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
   return (
