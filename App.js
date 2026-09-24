91c4a805587a33bdefa27f27182de82997bd9f8a
Deshan <deshananuruddha815@gmail.com>
2026-09-24 09:02:23 +0530

    Test Commit

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './src/navigation/RootNavigator';
 
+// Test Commit
+
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
